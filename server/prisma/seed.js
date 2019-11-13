f5b410279ba6a9e0bbeaf818fe38df064e1f66e6
Akshay Kadam <akshaykadam0904@gmail.com>
2019-11-13 12:19:56 +0530

    Reset seed.js to initial values

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -26,45 +26,11 @@ async function main() {
       streak: 60,
     },
   })
-  const callFam = await photon.habits.create({
-    data: {
-      name: 'Call Family',
-      streak: 63,
-    },
-  })
-  const swimming = await photon.habits.create({
-    data: {
-      name: 'Swimming',
-      streak: 7,
-    },
-  })
-  const tennis = await photon.habits.create({
-    data: {
-      name: 'Tennis',
-      streak: 18,
-    },
-  })
-  const noSmoking = await photon.habits.create({
-    data: {
-      name: 'No Smoking',
-      streak: 100,
-    },
-  })
-  const codeEveryday = await photon.habits.create({
-    data: {
-      name: 'Code Everyday',
-      streak: 309,
-    },
-  })
   console.log({
     workout,
     running,
     cycling,
     meditation,
-    callFam,
-    tennis,
-    noSmoking,
-    codeEveryday,
   })
 }
 
